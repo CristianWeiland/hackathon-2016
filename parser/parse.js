7ed2ea0a9601ec5debff053d6654281847e50a36
Lucas Romanó <luc4sromano@gmail.com>
2016-11-26 19:38:19 -0500

    initial parser
    Signed-off-by: Lucas Romanó <luc4sromano@gmail.com>

--- parser/parse.js
+++ parser/parse.js
@@ -3,12 +3,35 @@
 var csv = require('csv');
 var fs = require('fs');
 
-var parser = csv.parser();
+var parser = csv.parse({
+    delimiter: ';'
+    , columns: ['code', 'name' , 'category' , 'type', 'unity']
+    , auto_parse: true
+    , escape: '\\'
+    , quote: ''
+});
+var stringfier = csv.stringify();
 
-var readStream = fs.createReadStream('products.csv');
+var readStream = fs.createReadStream('products.csv',{ encoding : 'utf-8'}).pipe(parser);
 
-readStream.on('readble', function() {
-    while(data = readStream.read()) {
-        console.log(data);
+var parsed = {};
+
+parser.on('readable', function() {
+    while(data = parser.read()) {
+        parsed[data.code] = data.name;
     }
 });
+
+parser.on('error', function(error){
+    console.log(error);
+});
+
+parser.on('finish', function (){
+    console.log(parsed);
+});
+
+/*stringfier.on('readable', function() {
+    while(data = stringfier.read()) {
+        console.log(data);
+    }
+});*/
