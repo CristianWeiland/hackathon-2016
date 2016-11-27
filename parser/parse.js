ca1c7c788bc51d31c39128d50aa1b80bff59d0db
Lucas Romanó <luc4sromano@gmail.com>
2016-11-26 20:26:20 -0500

    parse of products ok
    Signed-off-by: Lucas Romanó <luc4sromano@gmail.com>

--- parser/parse.js
+++ parser/parse.js
@@ -5,20 +5,50 @@ var fs = require('fs');
 
 var parser = csv.parse({
     delimiter: ';'
-    , columns: ['code', 'name' , 'category' , 'type', 'unity']
+    , columns: true
     , auto_parse: true
     , escape: '\\'
     , quote: ''
 });
 var stringfier = csv.stringify();
 
-var readStream = fs.createReadStream('products.csv',{ encoding : 'utf-8'}).pipe(parser);
+var readStream = fs.createReadStream('registers.csv',{ encoding : 'utf-8'}).pipe(parser);
 
 var parsed = {};
 
+function extract (obj) {
+
+    var prod = {
+         nome: data['PRODUTO_DESCRICAO']
+         , unidade: data['TIPO_UNIDADE_MEDIDA_PRODUTO_DESCRICAO']
+         , unidade_sigla: data['TIPO_UNIDADE_MEDIDA_PRODUTO_SIGLA']
+    };
+    prod.nome = prod.nome.replace(/"/g, '\\"');
+    prod.unidade = (prod.unidade === 'KILOGRAMA') ? 'GRAMA' : prod.unidade;
+    prod.unidade_sigla = (prod.unidade_sigla === 'KG') ? 'GR' : prod.unidade_sigla;
+
+    return prod;
+}
+
 parser.on('readable', function() {
+    var prod, aux;
     while(data = parser.read()) {
-        parsed[data.code] = data.name;
+        prod = parsed[data['PRODUTO_DESCRICAO']];
+        if( prod == undefined) {
+            prod = extract(data);
+            console.log('INSERT INTO produto (nome, unidade, unidade_sigla)' +
+            ' VALUES ("' + prod.nome + '", "' + prod.unidade + '", "' + prod.unidade_sigla + '");');
+            parsed[prod.nome] = prod;
+        }
+        else {
+           aux = extract(data);
+            if(prod.unidade !== aux.unidade) {
+                console.log('FUCK1 ' + prod.nome + ' ' + prod.unidade + ' ' + aux.unidade);
+            }
+            if(prod.unidade_sigla !== aux.unidade_sigla) {
+                console.log('FUCK2 ' + prod.nome + ' ' + prod.unidade_sigla + ' ' + aux.unidade_sigla);
+            }
+        }
     }
 });
 
@@ -27,7 +57,7 @@ parser.on('error', function(error){
 });
 
 parser.on('finish', function (){
-    console.log(parsed);
+    // console.log(parsed);
 });
 
 /*stringfier.on('readable', function() {
